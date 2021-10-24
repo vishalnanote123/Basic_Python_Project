5b6f2a8be9930771e377cd1d52f33110c9b1d36a
Vishal Nanote <77392594+vishalnanote123@users.noreply.github.com>
2021-10-23 21:34:33 -0700

    Add files via upload

---
+++ Input sum.py
@@ -0,0 +1,8 @@
+# Python program for two input number
+numb1 = input("first number :")
+numb2 = input("\n second number:")
+# Adding two numbers
+# User might also enter float number
+sum = float(numb1) + float(numb2)
+
+print("sum of {0} and {1} is {2}:".format(numb1, numb2, sum))
